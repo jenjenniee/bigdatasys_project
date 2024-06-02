c6488c9b3ee872a74104a54c0e730da2c4737ef8
lee jin gyeong <jenjennie707@gmail.com>
2024-06-02 22:46:48 +0900

    쿼리 추출, 쿼리 시각화 이미지, 검색 기능추가

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
 from pymongo import MongoClient
+import ast
+import os
 
 app = Flask(__name__)
 
+# MongoDB 설정
+client = MongoClient('mongodb://localhost:27017/')
+db = client['delivery']
+collection = db['deliveries']
+
+# 쿼리 제목과 넘버 매핑 딕셔너리
+query_title_to_number = {
+    "Rank of delivery food items across all periods and regions": "query7",
+    # 다른 쿼리들의 제목과 넘버 추가
+}
+
 @app.route('/')
-@app.route('/home', methods =['POST'])
-def home():
-    client = MongoClient('mongodb://localhost:27017/')
+def index():
+    query_title = request.args.get('query_value')
+    results = []
 
-    # db접근
-    db = client.practice
-    # collection접근
-    collection = db.students
+    if query_title:
+        query_number = query_title_to_number.get(query_title)
+        if query_number:
+            # 해당 쿼리 넘버에 해당하는 이미지 반환
+            return render_template('index.html', results=results, query_title=query_title, query_number=query_number)
 
-    # 임시쿼리 - collect에서 모든 데이터 가져오기
-    result = collection.find()
+    return render_template('index.html', results=results, query_title=query_title)
 
-
-
-    # client.close()
-
-    return render_template('index.html', students=result)
+@app.route('/img/<query_number>')
+def get_image(query_number):
+    # 이미지가 있는 현재 디렉토리의 경로 설정
+    image_directory = os.path.join(os.getcwd(), 'img')
+    # 이미지를 해당 경로에서 가져와서 반환
+    return send_from_directory(image_directory, f"{query_number}.png")
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
